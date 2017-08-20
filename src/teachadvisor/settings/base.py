021c595475a702d18a29d7a469bfd2fa6c59dd35
Piyush Maurya <piyush.maurya@live.in>
2017-08-20 11:31:16 +0530

    Tutorial video successfully added on tutorial page

--- src/teachadvisor/settings/base.py
+++ src/teachadvisor/settings/base.py
@@ -161,8 +161,8 @@ WSGI_APPLICATION = 'teachadvisor.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+# # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
