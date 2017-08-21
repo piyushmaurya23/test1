20363f67ece1721f68eedebe7daaee44de801f66
Piyush Maurya <piyush.maurya@live.in>
2017-08-21 21:36:17 +0530

    text alignment done, features text looks better than ever.

--- src/teachadvisor/settings/base.py
+++ src/teachadvisor/settings/base.py
@@ -163,25 +163,25 @@ WSGI_APPLICATION = 'teachadvisor.wsgi.application'
 # Database
 # # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # confidential
 # # localhost - need to install postgres under pip install and start a new database in the
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
-        'USER': 'admin',
-        'PASSWORD': 'qwer1234',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db',
+#         'USER': 'admin',
+#         'PASSWORD': 'qwer1234',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 # Password validation
